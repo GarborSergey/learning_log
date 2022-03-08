28b2f036bc40a730a1e6aea8842428e64e2f66b4
GarborSerey <garborfersru@gmail.com>
2022-03-08 22:16:20 +0200

    Занятие 1

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     # Страница со всеми темаи
     path('topics/', views.topics, name='topics'),
     # Страница с подробной информацией по отдельной теме
-    path('topics/<int:topic_id>', views.topic, name='topic') #<int:post_id>
+    path('topics/<int:topic_id>', views.topic, name='topic'),  #<int:post_id>
+    # Страница с добовлением новой темы
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Страница с добовлением новой записи к теме
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,10 @@
 # Представления сайта
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Topic
+from django.urls import reverse
+from .forms import TopicForm, EntryForm
+
 
 def index(request):
     """Домашняя страница приложения learning log"""
@@ -21,3 +24,59 @@ def topic(request, topic_id):
     # запрос 2 к БД
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему"""
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма
+        form = TopicForm()
+    else:
+        # Отправленны данные POST; обработать данные
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context ={'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Добавляет новую запись по конкретной теме"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма
+        form = EntryForm()
+    else:
+        # Отправленны данные POST; обработать данные
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+# def new_entry(request, topic_id):
+#     """ Add new entry for specific topic """
+#     topic = Topic.objects.get(id = topic_id)
+#
+#     # refactored
+#     check_topic_owner(topic, request)
+#
+#     if (request.method != 'POST' and request.method == 'GET'):
+#         # No POST data submitted, return blank form
+#         form = EntryForm()
+#
+#     else:
+#         # POST data exists, process data within request.POST
+#         form = EntryForm(data = request.POST)
+#         if form.is_valid():
+#             new_entry = form.save(commit=False)
+#             new_entry.topic = topic
+#             new_entry.save()
+#             return redirect('learning_logs:topic', topic_id=topic_id)
+#
+#     # Display blank or invalid form
+#     context = {'topic': topic, 'form': form}
+#     return render(request, 'learning_logs/new_entry.html', context)
