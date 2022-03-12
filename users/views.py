b62a2ccd54972453aadfb5f306bf76efe5e9c2e0
GarborSerey <garborfersru@gmail.com>
2022-03-12 13:05:42 +0200

    Занятие 4 сделал пользователей

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.urls import  reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+# Проще всего воспользоваться UserCreationForm и представлением на основе класса CreateView,
+# предоставляемыми Django.
+# UserCreationForm - это класс ModelForm для создания нового пользователя, который генерирует требуемые поля,
+# а именно username и password.
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+# Добавляю класс представления
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
